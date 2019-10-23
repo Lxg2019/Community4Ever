ca8e383521248803e957058ebd7528f8b48b046e
Lxg2019 <54814624+Lxg2019@users.noreply.github.com>
2019-10-23 19:18:33 +0200

    Add files via upload

---
+++ Models/Adress/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsedClass.models.Stadt
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        public string CountryName { get; set; }
+    }
+}
---
+++ Models/Adress/OrderPlace.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsedClass.models.Stadt
+{
+    public class OrderPlace:Adress
+    {
+    }
+}
