2f6f45cdacdd548156197f6aedfd6b4148449393
Lxg2019 <54814624+Lxg2019@users.noreply.github.com>
2019-10-23 19:11:55 +0200

    Create Role.cs

---
+++ Models/Role/Role.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsedClass.models.role
+{
+    public class Role
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } // administrator, user, prodiver, taker, driver,
+
+    }
+}
