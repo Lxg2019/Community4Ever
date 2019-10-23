af93249bfac60cd128fd7dbec764ba3bb190867a
Lxg2019 <54814624+Lxg2019@users.noreply.github.com>
2019-10-23 19:16:31 +0200

    Add files via upload

---
+++ Models/Order/OrderContent/ProcurementService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsedClass.models.Stadt;
+
+namespace UsedClass.models.order.OrderOfContent
+{
+    public class ProcurementService:OrderContent
+    {
+        public DateTime TakeTime { get; set; }
+        public Adress TakeAdress { get; set; }
+
+        public string ProcurementDetails { get; set; }
+    }
+}
---
+++ Models/Order/OrderContent/Travel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsedClass.models.Stadt;
+
+namespace UsedClass.models.order.OrderOfContent
+{
+    public class Travel
+    {
+        public DateTime End { get; set; }
+
+        public virtual OrderPlace Orderplace{ get; set; }
+
+        public string OrderDetails { get; set; }
+    }
+}
