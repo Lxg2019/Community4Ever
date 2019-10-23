c8b959325a47a79229f07560953c96224edd60d4
Lxg2019 <54814624+Lxg2019@users.noreply.github.com>
2019-10-23 19:09:02 +0200

    Update Person.cs

--- Models/Person/Person.cs
+++ Models/Person/Person.cs
@@ -1,1 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsedClass.models.role;
 
+namespace UsedClass.models.person
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        public ContactType ContactType { get; set; }
+
+        public string ContactTypeString { get; set; }
+
+        public string Telefon { get; set; }
+
+        public Role Role { get; set; }
+
+        public int RoleId { get; set; }
+    }
+}
