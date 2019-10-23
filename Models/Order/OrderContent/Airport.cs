c166d3d94bedd99c905ef759245112b56253cfe8
Lxg2019 <54814624+Lxg2019@users.noreply.github.com>
2019-10-23 19:16:13 +0200

    Update Airport.cs

--- Models/Order/OrderContent/Airport.cs
+++ Models/Order/OrderContent/Airport.cs
@@ -1,1 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsedClass.models.Stadt;
+using UsedClass.models.vehicle;
 
+namespace UsedClass.models.order.OrderOfContent
+{
+    public class Airport:OrderContent
+    {
+
+        public Adress FromAdress { get; set; }
+
+        public Adress ToAdress { get; set; }
+
+        public virtual Vehicle Vehcile { get; set; }
+
+        public int AmountofPackage { get; set; }
+
+
+    }
+}
